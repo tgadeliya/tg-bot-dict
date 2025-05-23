9b4c44aed5649135de688f14e95787d7f2c88ca0
Tsimur hadeliya <tgadeliya@gmail.com>
2025-05-23 22:58:59 +0300

    update

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def main():
-    print("Hello from tg-bot-dict!")
+import logging
 
+from telegram import ForceReply, Update
+from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
 
 if __name__ == "__main__":
-    main()
+    
+    print("Hi!")

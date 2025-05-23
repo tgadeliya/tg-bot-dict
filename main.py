9b72a9903352058edc31fbf90bb3cce95a41599b
Tsimur Hadeliya <tgadeliya@gmail.com>
2025-05-23 23:17:07 +0300

    Test version that runs locally!

--- main.py
+++ main.py
@@ -1,8 +1,34 @@
 import logging
+import os
 
+from dotenv import load_dotenv
 from telegram import ForceReply, Update
 from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
 
-if __name__ == "__main__":
+load_dotenv()
+TELEGRAM_BOT_TOKEN: str| None = os.getenv("TELEGRAM_BOT_TOKEN")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    user = update.effective_user
+    await update.message.reply_html(
+        f"Hi {user.mention_html()}!"
+    )
+
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Echo the user message."""
+    user_message = update.message.text
+    await update.message.reply_text(f"You said: {user_message}")
+
+def main():
     
-    print("Hi!")
+    app = Application.builder().token(TELEGRAM_BOT_TOKEN).build()
+
+    app.add_handler(CommandHandler("start", start))
+
+
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+    app.run_polling(allowed_updates=Update.ALL_TYPES)
+    
+
+if __name__ == "__main__":
+    main()
